7a39161076de78a4b0200cdc7761dff4ffa1c57a
Helyomar Lins <111030700+HelyomarLins@users.noreply.github.com>
2024-05-13 21:34:22 -0300

    Implementando paginas e rotas

--- angular-crud/src/app/_components/listar-disciplina/listar-disciplina.component.ts
+++ angular-crud/src/app/_components/listar-disciplina/listar-disciplina.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
-  selector: 'app-lista-disciplina',
-  templateUrl: './lista-disciplina.component.html',
-  styleUrl: './lista-disciplina.component.scss'
+  selector: 'app-listar-disciplina',
+  templateUrl: './listar-disciplina.component.html',
+  styleUrls: ['./listar-disciplina.component.scss']
 })
-export class ListaDisciplinaComponent {
+export class ListaDisciplinaComponent implements OnInit {
+  disciplinas: any[] = []; // Array para armazenar as disciplinas obtidas do servidor
 
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    // Método executado quando o componente é inicializado
+    this.listarDisciplinas(); // Chama a função para listar as disciplinas
+  }
+
+  listarDisciplinas() {
+    // Função para obter a lista de disciplinas do servidor
+    this.http.get<any>('http://localhost/Angular-Crud/Angular-PHP-Crud/angular-crud/API/listar_disciplina.php')
+      .subscribe(
+        (response) => {
+          // Callback para tratamento da resposta bem-sucedida
+          console.log('Resposta do servidor:', response); // Exibe a resposta do servidor no console
+          if (response.status === 'success') {
+            this.disciplinas = response.disciplinas; // Preenche a variável disciplinas com os dados obtidos do servidor
+          } else {
+            console.error('Erro ao obter lista de disciplinas:', response.message); // Exibe mensagem de erro no console
+          }
+        },
+        (error) => {
+          // Callback para tratamento de erro na requisição
+          console.error('Erro ao obter lista de disciplinas:', error); // Exibe mensagem de erro no console
+        }
+      );
+  }
 }
--- angular-crud/src/app/app-routing.module.ts
+++ angular-crud/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CadastroDisciplinaComponent } from './_components/cad-disciplina/cad-disciplina.component';
+import { ListaDisciplinaComponent } from './_components/listar-disciplina/listar-disciplina.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'cadastrar-disciplina', component: CadastroDisciplinaComponent },
+  { path: 'listar-disciplinas', component: ListaDisciplinaComponent },
+  { path: '', redirectTo: '/cadastrar-disciplina', pathMatch: 'full' } // Rota padrão redireciona para cadastrar-disciplina
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
