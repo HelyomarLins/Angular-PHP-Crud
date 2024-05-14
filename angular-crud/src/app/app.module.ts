7eb35f326d8fea38a96c76fb07e7304bf595795c
Helyomar Lins <111030700+HelyomarLins@users.noreply.github.com>
2024-05-13 21:50:01 -0300

    .

--- angular-crud/src/app/app.module.ts
+++ angular-crud/src/app/app.module.ts
@@ -6,7 +6,7 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CadastroDisciplinaComponent } from './_components/cad-disciplina/cad-disciplina.component';
-import { ListaDisciplinaComponent } from './_components/lista-disciplina/lista-disciplina.component';
+import { ListaDisciplinaComponent } from './_components/listar-disciplina/listar-disciplina.component';
 
 
 @NgModule({
