3b58394899813d61c515bfba76d06fdb48c23bb6
Helyomar Lins <111030700+HelyomarLins@users.noreply.github.com>
2024-05-14 16:12:05 -0300

    implementando modal
    Modal de acesso ao programa..

--- angular-crud/src/app/_components/pages/_home/home.component.ts
+++ angular-crud/src/app/_components/pages/_home/home.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit, ElementRef } from '@angular/core';
+declare var bootstrap: any;
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
+export class HomeComponent implements AfterViewInit {
+  constructor(private elementRef: ElementRef) {}
 
+  ngAfterViewInit(): void {
+    const modalElement = this.elementRef.nativeElement.querySelector('#staticBackdrop1');
+    if (modalElement) {
+      const modalInstance = new bootstrap.Modal(modalElement);
+      modalInstance.show();
+    } else {
+      console.error('Modal element not found.');
+    }
+  }
 }
