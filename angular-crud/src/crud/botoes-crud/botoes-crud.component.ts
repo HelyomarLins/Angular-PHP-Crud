b2c1ded6f585c2f6d107f156ef8400d56e26f445
Helyomar Lins <111030700+HelyomarLins@users.noreply.github.com>
2024-05-16 12:06:49 -0300

    criando trotas n onavbar

---
+++ angular-crud/src/crud/botoes-crud/botoes-crud.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-botoes-crud',
+  templateUrl: './botoes-crud.component.html',
+  styleUrl: './botoes-crud.component.scss'
+})
+export class BotoesCrudComponent {
+
+}
--- angular-crud/src/crud/crud.module.ts
+++ angular-crud/src/crud/crud.module.ts
@@ -9,13 +9,15 @@ import { CrudComponent } from './crud.component'; // Certifique-se de importar s
 import { NavbarComponent } from '../app/_components/pages/navbar/navbar.component';
 import { BaseUiComponent } from '../app/_components/base-ui/base-ui.component';
 import { FooterComponent } from '../app/_components/pages/footer/footer.component';
+import { BotoesCrudComponent } from './botoes-crud/botoes-crud.component';
 // Importação e declaração de componentes CRUD
 @NgModule({
   declarations: [
     CrudComponent,
     NavbarComponent,
     BaseUiComponent,
-    FooterComponent
+    FooterComponent,
+    BotoesCrudComponent
   ],
   imports: [
     CommonModule,
