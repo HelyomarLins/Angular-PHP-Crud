56770027d55828df564a2a40e0c5bf3b3382ca37
Helyomar Lins <111030700+HelyomarLins@users.noreply.github.com>
2024-05-17 00:17:28 -0300

    .

--- angular-crud/src/app/app.module.ts
+++ angular-crud/src/app/app.module.ts
@@ -1,22 +1,28 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './_components/pages/navbar/navbar.component';
+import { BaseUiComponent } from './_components/base-ui/base-ui.component';
+import { FooterComponent } from './_components/pages/footer/footer.component';
 import { HomeComponent } from './_components/pages/_home/home.component';
+import { CrudModule } from '../crud/crud.module'; // Importe o CrudModule aqui
 
 @NgModule({
   declarations: [
     AppComponent,
+    NavbarComponent,
+    BaseUiComponent,
+    FooterComponent,
     HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- angular-crud/src/crud/crud.module.ts
+++ angular-crud/src/crud/crud.module.ts
@@ -1,38 +1,30 @@
 // crud/crud.module.ts
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms'; // Mantenha apenas o FormsModule
-import { HttpClientModule } from '@angular/common/http';
 import { CrudRoutingModule } from './crud-routing.module';
 import { CrudComponent } from './crud.component';
-import { NavbarComponent } from '../app/_components/pages/navbar/navbar.component';
-import { BaseUiComponent } from '../app/_components/base-ui/base-ui.component';
-import { FooterComponent } from '../app/_components/pages/footer/footer.component';
 import { BotoesCrudComponent } from './botoes-crud/botoes-crud.component';
 import { RouterModule } from '@angular/router';
-
+import { FormsModule } from '@angular/forms';
+import { ListarAlunosComponent } from './listar-alunos/listar-alunos.component';
 @NgModule({
   declarations: [
     CrudComponent,
-    NavbarComponent,
-    BaseUiComponent,
-    FooterComponent,
-    BotoesCrudComponent,
+    ListarAlunosComponent,
+    BotoesCrudComponent
   ],
   imports: [
     CommonModule,
     FormsModule,
-    HttpClientModule,
     CrudRoutingModule,
+    FormsModule,
     RouterModule
   ],
   exports: [
     CrudComponent,
-    NavbarComponent,
-    BaseUiComponent,
-    FooterComponent,
-    BotoesCrudComponent,
-    RouterModule
-  ]
+    ListarAlunosComponent,
+    BotoesCrudComponent
+    ],
+    schemas: [ CUSTOM_ELEMENTS_SCHEMA ]
 })
 export class CrudModule { }
